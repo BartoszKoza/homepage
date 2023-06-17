10b7a99ce5d1e8d8ed67c9cf7979c77c3a044781
Bartosz Koza <kozabartosz01@gmail.com>
2023-06-17 14:43:51 +0200

    The code has been organized.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
-console.log("Cześć!");
+{
+    const welcome = () => {
+        console.log("Cześć!");
+    }
 
-let button = document.querySelector(".button__headerRemove");
-let header = document.querySelector(".header__title");
-
-button.addEventListener("click", () => {
-    header.remove();
-})
-
-
-let body = document.querySelector(".body");
-let dark_mode = document.querySelector(".button__darkMode");
-let themeName = document.querySelector(".themeName");
-dark_mode.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    themeName.innerText = body.classList.contains("dark") ? "Białe" : "Szare"});
-
-
+    const toggleBackground = () => {
+        const themeName = document.querySelector(".themeName");
+        const body = document.querySelector(".body");
+        body.classList.toggle("dark");
+        themeName.innerText = body.classList.contains("dark") ? "Białe" : "Szare"
+    };
+    
+    const init = () => {
+        const button = document.querySelector(".button__headerRemove");
+        const header = document.querySelector(".header__title");
+    
+        button.addEventListener("click", () => {
+        header.remove();
+        });
+        
+        const dark_mode = document.querySelector(".button__darkMode");
+        dark_mode.addEventListener("click", toggleBackground);
+        
+        welcome();
+    }
+    init ();
+     
+}
